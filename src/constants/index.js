3b5762bb6d683d414f8984b500f7a1282ddcd92d
Maruria Joseph <josephmaruria@gmail.com>
2023-01-09 23:37:42 +0300

    progress

---
+++ src/constants/index.js
@@ -0,0 +1,201 @@
+import { facebook, instagram, linkedin, twitter, airbnb, binance, coinbase, dropbox, send, shield, star, fashion, meal, media, calendar } from "../assets";
+
+export const navLinks = [
+  {
+    id: "home",
+    title: "Home",
+  },
+  {
+    id: "features",
+    title: "Features",
+  },
+  {
+    id: "product",
+    title: "Product",
+  },
+  {
+    id: "clients",
+    title: "Clients",
+  },
+];
+
+export const features = [
+  {
+    id: "feature-1",
+    icon: star,
+    title: "Rewards",
+    content:
+      "The best credit cards offer some tantalizing combinations of promotions and prizes",
+  },
+  {
+    id: "feature-2",
+    icon: shield,
+    title: "100% Secured",
+    content:
+      "We take proactive steps make sure your information and transactions are secure.",
+  },
+  {
+    id: "feature-3",
+    icon: send,
+    title: "Balance Transfer",
+    content:
+      "A balance transfer credit card can save you a lot of money in interest charges.",
+  },
+];
+
+export const feedback = [
+  {
+    id: "feedback-1",
+    content:
+      "Discover & Explore our online Fashion Platform DadaSoko.",
+    name: "DadaSoko.com",
+    title: "A global eCommerce marketplace structured for young women entrepreneurs.",
+    img: fashion,
+  },
+  {
+    id: "feedback-2",
+    content:
+      "Discover & Explore Our Food Ordering Platform Mealsoko.",
+    name: "MealSoko.com",
+    title: "Order your desired meals from your favorite vendorks trhrough MealSoko.",
+    img: meal,
+  },
+  {
+    id: "feedback-3",
+    content:
+      "Discover & Explore our Marketing Platform EarnSoko.",
+    name: "EarnSoko.com",
+    title: "Earn for being an influencer in the SokoVerse where everyone is an influencer.",
+    img: media,
+  },
+  {
+    id: "feedback-4",
+    content:
+      "Discover & Explore our Event Ticketing Platform TicketSoko.",
+    name: "TicketSoko.com",
+    title: "Buy your event tickets directly through the TicketSoko platform.",
+    img: calendar,
+  },
+];
+
+export const stats = [
+  {
+    id: "stats-1",
+    title: "Clients",
+    value: "200+",
+  },
+  {
+    id: "stats-2",
+    title: "Integrations",
+    value: "500+",
+  },
+  {
+    id: "stats-3",
+    title: "User Support",
+    value: "24/7",
+  },
+];
+
+export const footerLinks = [
+  {
+    title: "Navigation",
+    links: [
+      {
+        name: "Home",
+        link: "https://www.hoobank.com/content/",
+      },
+      {
+        name: "About",
+        link: "https://www.hoobank.com/how-it-works/",
+      },
+      {
+        name: "Services",
+        link: "https://www.hoobank.com/create/",
+      },
+      {
+        name: "Contact",
+        link: "https://www.hoobank.com/explore/",
+      },
+    ],
+  },
+  {
+    title: "Services",
+    links: [
+      {
+        name: "DadaSoko",
+        href: "https://ke.dadasoko.com/",
+      },
+      {
+        name: "TicketSoko",
+        link: "https://ke.dadasoko.com/",
+      },
+      {
+        name: "MealSoko",
+        link: "https://ke.dadasoko.com/",
+      },
+      {
+        name: "EarnSoko",
+        link: "https://ke.dadasoko.com/",
+      },
+      {
+        name: "PaySoko",
+        link: "https://ke.dadasoko.com/",
+      },
+    ],
+  },
+  {
+    title: "Partner",
+    links: [
+      {
+        name: "Our Partner",
+        link: "https://www.hoobank.com/our-partner/",
+      },
+      {
+        name: "Become a Partner",
+        link: "https://www.hoobank.com/become-a-partner/",
+      },
+    ],
+  },
+];
+
+export const socialMedia = [
+  {
+    id: "social-media-1",
+    icon: instagram,
+    link: "https://www.instagram.com/",
+  },
+  {
+    id: "social-media-2",
+    icon: facebook,
+    link: "https://www.facebook.com/",
+  },
+  {
+    id: "social-media-3",
+    icon: twitter,
+    link: "https://www.twitter.com/",
+  },
+  {
+    id: "social-media-4",
+    icon: linkedin,
+    link: "https://www.linkedin.com/",
+  },
+];
+
+export const clients = [
+  {
+    id: "client-1",
+    logo: airbnb,
+  },
+  {
+    id: "client-2",
+    logo: binance,
+  },
+  {
+    id: "client-3",
+    logo: coinbase,
+  },
+  {
+    id: "client-4",
+    logo: dropbox,
+  },
+];
